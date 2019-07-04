5b51b745e8b9cd03c2b179436fd3f42269bb6b07
Bifurcar <IanEzequielG@pm.me>
2019-07-04 18:12:35 -0300

    Finalizar seeder de Configuracion

--- database/factories/ConfiguracionFactory.php
+++ database/factories/ConfiguracionFactory.php
@@ -6,9 +6,11 @@ use App\Configuracion;
 use Faker\Generator as Faker;
 
 $factory->define(Configuracion::class, function (Faker $faker) {
+	static $indice = 0;
+	$jornada = App\Jornada::all();
     return [
-        'jornada_id' => App\Jornada::all()->unique()->id,
-        'cantidad_asistencias' => $faker->numberBetween(3,10),
+        'jornada_id' => $jornada[$indice++]->id,
+        'cantidad_asistencias' => $faker->numberBetween(5,10),
         'tolerancia' => $faker->numberBetween(30,60),
     ];
 });
